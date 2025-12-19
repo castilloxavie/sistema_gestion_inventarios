04c56078d9163d2a87ca91687b0f698b7378c113
castilloxavie <xcastillovaron93@gmail.com>
2025-12-19 14:39:43 -0500

    fix(models): añadir el campo producto_id al modelo InventoryMovement
    El campo producto_id faltaba en el modelo InventoryMovement, necesario para el seguimiento de los movimientos de productos. También se deshabilitó la sincronización automática de tablas en el modo de desarrollo para evitar cambios de esquema no deseados.

--- backend/src/models/InventoryMovementModels.js
+++ backend/src/models/InventoryMovementModels.js
@@ -4,6 +4,7 @@ import { sequelize } from "../config/databases.js";
 export const InventoryMovement = sequelize.define(
     "InventoryMovement",
     {
+        producto_id: { type: DataTypes.INTEGER,  allowNull: true},
         tipo: {type: DataTypes.ENUM("entrada", "salida"), allowNull: false},
         cantidad: {type: DataTypes.INTEGER, allowNull: false},
         descripcion: { type: DataTypes.STRING}
