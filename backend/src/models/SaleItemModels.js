97cfe2474093475f79e3027190828ec9125f6db1
castilloxavie <xcastillovaron93@gmail.com>
2025-12-19 20:03:15 -0500

    estilo(modelos): Reformatear el modelo SaleItem para una mejor legibilidad.
    Se aplicó una sangría y un espaciado consistentes para mejorar la estructura del código y su facilidad de mantenimiento.

--- backend/src/models/SaleItemModels.js
+++ backend/src/models/SaleItemModels.js
@@ -3,12 +3,13 @@ import { sequelize } from "../config/databases.js";
 
 
 export const SaleItem = sequelize.define(
-    "SaleItem",
-    {
-        cantidad:{type: DataTypes.INTEGER, allowNull: false},
-        precio_unitario: {type: DataTypes.DECIMAL(10, 2), allowNull: false},
-        venta_id: {type: DataTypes.INTEGER, allowNull: false},
-        producto_id: {type: DataTypes.INTEGER, allowNull: false}
-    },
-    {timestamps: true}
-)
+  "SaleItem",
+  {
+    cantidad: { type: DataTypes.INTEGER, allowNull: false },
+    precio_unitario: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
+    venta_id: { type: DataTypes.INTEGER, allowNull: false },
+    producto_id: { type: DataTypes.INTEGER, allowNull: false }
+  },
+  { timestamps: true }
+);
+
